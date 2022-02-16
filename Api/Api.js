a9f8efb9b31869c24335bac1a45b3fdd7ddd3310
PANKAJ KUMAR VISHWAKARMA <92736385+VISHWAKARMAJ@users.noreply.github.com>
2022-02-16 18:30:16 +0530

    d d

--- Api/Api.js
+++ Api/Api.js
@@ -1,8 +1,41 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Hello() {
-  useEffect(() => {}, []);
+  const [data, setData] = useState([]);
+  const Api = "https://jsonplaceholder.typicode.com/users";
 
-  return <></>;
+  useEffect(() => {
+    fetch(Api)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  // useEffect(() => {}, []);
+  return (
+    <>
+      {/* <h2> hello</h2> */}
+      {data.map((obj) => {
+        return (
+          // <li>
+          <div>
+            <li>
+              {obj.name}({obj.username}) email:{obj.email}
+              {obj.address.street}
+              {obj.address.suite}
+              {obj.address.city}
+              {obj.address.zipcode}
+              {obj.address.geo.lat}
+              {obj.address.geo.lng}
+              {obj.phone}
+              {obj.website}
+              {obj.company.name}
+              {obj.company.catchPhrase}
+              {obj.company.bs}
+            </li>
+          </div>
+          // </li>
+        );
+      })}{" "}
+    </>
+  );
 }
